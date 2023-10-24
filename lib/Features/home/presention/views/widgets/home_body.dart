5fd788188aa2a062a5b2416572964cdeb731582a
Your Name <you@example.com>
2023-10-24 09:44:45 -0700

    add image to best seller list view

--- lib/Features/home/presention/views/widgets/home_body.dart
+++ lib/Features/home/presention/views/widgets/home_body.dart
@@ -1,5 +1,6 @@
 import 'package:booklyapp/Features/home/presention/views/widgets/custom_appBar.dart';
 import 'package:booklyapp/Features/home/presention/views/widgets/list%20viewItem.dart';
+import 'package:booklyapp/core/utils/assets.dart';
 import 'package:booklyapp/core/utils/style.dart';
 
 import 'package:flutter/material.dart';
@@ -17,10 +18,39 @@ class HomeBody extends StatelessWidget {
           CustomAppBar(),
           ListViewItem(),
             SizedBox(height: 50,),
-            Text('Best Seller',style: StylesText.styleText,)
+            Text('Best Seller',style: StylesText.styleText,),
+            BestSellerListView(),
         ],
       ),
     );
+  }
+}
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+      aspectRatio: 2.5/4,
+      child: Container(
+        decoration: BoxDecoration(
+          image:const DecorationImage(image: AssetImage(Asset2.testImage),fit: BoxFit.fill),
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.red,
+        ),
+      ),
+    ),
+    Column(
+      children: [
+        
+      ],
+    )
+        ],
+      ));
   }
 }
 

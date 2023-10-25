feea79d36fb6f3cfc0a26938d87fd5f262627cc9
Your Name <you@example.com>
2023-10-25 10:53:58 -0700

    add image  for book details

--- lib/Features/home/presention/views/widgets/book_detail_view_body.dart
+++ lib/Features/home/presention/views/widgets/book_detail_view_body.dart
@@ -1,16 +1,21 @@
 import 'package:booklyapp/Features/home/presention/views/widgets/customDetailAppBar.dart';
+import 'package:booklyapp/Features/home/presention/views/widgets/customItem.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailViewBody extends StatelessWidget {
   const BookDetailViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 30),
+    var width = MediaQuery.of(context).size.width;
+    return  Padding(
+      padding:const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomBookDetailAppBar(),
+          const CustomBookDetailAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width*.17),
+            child:const CustomListViewItem(),
+          )
         ],
       ),
     );
--- lib/Features/home/presention/views/widgets/home_body.dart
+++ lib/Features/home/presention/views/widgets/home_body.dart
@@ -10,7 +10,8 @@ class HomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return const  CustomScrollView(
+      physics:  BouncingScrollPhysics(),
       slivers: [
         SliverToBoxAdapter(
           child:Column(

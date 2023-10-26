6d59160f42b454025e917732a0a88d846e4631df
Your Name <you@example.com>
2023-10-26 11:56:14 -0700

    Either class and return for method in home repo

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchFeatureBooks();
-  fetchBestSellerBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeatureBooks();
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
 }

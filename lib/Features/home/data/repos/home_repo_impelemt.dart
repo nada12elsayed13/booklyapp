5b110a4ad707d5e9c3ebf4b8bc3a0da764944016
Your Name <you@example.com>
2023-11-03 11:24:26 -0700

    create api service

---
+++ lib/Features/home/data/repos/home_repo_impelemt.dart
@@ -0,0 +1,19 @@
+import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/home/data/repos/home_repo.dart';
+import 'package:booklyapp/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoIm implements HomeRepo{
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
+    // TODO: implement fetchFeatureBooks
+    throw UnimplementedError();
+  }
+
+}

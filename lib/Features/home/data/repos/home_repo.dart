11c6ba38e7769356efbae32682e10578038a1c09
Your Name <you@example.com>
2023-11-03 11:58:10 -0700

    implement dio

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:booklyapp/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failure,List<BookModel>>> fetchFeatureBooks();
-  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewstBooks();
 }
--- lib/Features/home/data/repos/home_repo_impelemt.dart
+++ lib/Features/home/data/repos/home_repo_impelemt.dart
@@ -1,13 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+
 import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
 import 'package:booklyapp/Features/home/data/repos/home_repo.dart';
 import 'package:booklyapp/core/errors/failure.dart';
-import 'package:dartz/dartz.dart';
+import 'package:booklyapp/core/utils/api_service.dart';
 
-class HomeRepoIm implements HomeRepo{
+class HomeRepoIm implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoIm(
+    this.apiService,
+  );
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewstBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
@@ -15,5 +32,4 @@ class HomeRepoIm implements HomeRepo{
     // TODO: implement fetchFeatureBooks
     throw UnimplementedError();
   }
-
 }

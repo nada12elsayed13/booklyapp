abc108450a68839ad60e19f2beb34168fd03e2d8
Your Name <you@example.com>
2023-12-03 08:58:25 -0800

    feature books cubit method

--- lib/Features/home/presention/manager/futer_Book_Cubit/feature_books_cubit_cubit.dart
+++ lib/Features/home/presention/manager/futer_Book_Cubit/feature_books_cubit_cubit.dart
@@ -1,9 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bloc/bloc.dart';
-import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
+
+import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/home/data/repos/home_repo.dart';
 
 part 'feature_books_cubit_state.dart';
 
 class FeatureBooksCubitCubit extends Cubit<FeatureBooksCubitState> {
-  FeatureBooksCubitCubit() : super(FeatureBooksCubitInitial());
+  FeatureBooksCubitCubit(
+    this.homeRepo,
+  ) : super(FeatureBooksCubitInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> featureBooks() async {
+    emit(FeatureBooksCubitLoading());
+    var result = await homeRepo.fetchFeatureBooks();
+    result.fold((failuer) {
+      emit(FeatureBooksCubitFailuer(failuer.errorMessage));
+    }, (books) {
+      emit(FeatureBooksCubitSucesses(books));
+    });
+  }
 }

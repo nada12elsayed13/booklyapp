70433964ba9f605d1d4bd400f678e4bb116b0581
Your Name <you@example.com>
2023-12-04 11:13:17 -0800

    chaced network image

--- lib/Features/home/presention/views/widgets/customItem.dart
+++ lib/Features/home/presention/views/widgets/customItem.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:booklyapp/core/widget/customcircleindicator.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import 'package:booklyapp/core/utils/assets.dart';
@@ -11,15 +13,16 @@ class CustomListViewItem extends StatelessWidget {
   final String imagUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          image:  DecorationImage(
-              image: NetworkImage(imagUrl), fit: BoxFit.fill),
-          borderRadius: BorderRadius.circular(16),
-          
-        ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imagUrl,
+          errorWidget: (context, url, error) =>const Icon(Icons.error),
+          placeholder: (context, url) =>const CustomCircleIndicator(),
+          )
       ),
     );
   }
